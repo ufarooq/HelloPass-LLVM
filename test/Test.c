7607bef79f2db4afc5ed2c94a57f535abe7beed8
Zhijia Zhao <emailzhijia@gmail.com>
2021-01-31 17:59:08 -0800

    Update Test.c

--- test/Test.c
+++ test/Test.c
@@ -2,12 +2,7 @@
 void test(int a, int b, int c, int d, int e, int f) {
         a = b * c;
         d = b;
-        b = b +1;
+        b = b + 1;
         e = d * c;
         f = b * c;
-}
-
-int main(int argc, char** argv) {
-	test(10, 11, 25, 33, 43,26);
-  return 0;
 }
